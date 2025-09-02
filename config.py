91c416d9626d8f17e9e2800c231d40a9619d3b55
UHD Official <uhdnetwork@gmail.com>
2025-09-02 16:14:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+import os
 
+class Config:
+    # Telegram Bot Config
+    API_ID = int(os.getenv("API_ID", ""))         # your API ID
+    API_HASH = os.getenv("API_HASH", "")  # your API Hash
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "")  # your Bot Token
+
+    # Other Configs
+    PORT = int(os.getenv("PORT", "8080"))
+    ADMIN = int(os.getenv("ADMIN", ""))  # your telegram user id
