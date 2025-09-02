7de9c18091e574713bdef33396a5ff28fad177d9
UHD Official <uhdnetwork@gmail.com>
2025-09-02 16:16:28 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,23 @@
 from aiohttp import web
+import time
+from config import Config
 
 routes = web.RouteTableDef()
 
+START_TIME = time.time()
+
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("RknDeveloper")
+    uptime = round(time.time() - START_TIME)
+    return web.json_response({
+        "status": "running",
+        "bot": "UHD-MediaHost-Bot",
+        "uptime_seconds": uptime,
+        "admin": Config.ADMIN if Config.ADMIN else "Not Set"
+    })
 
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
-  
