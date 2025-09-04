3830ea8e9d4a79cd2c09573513b23fc612652ff1
UHD Official <uhdnetwork@gmail.com>
2025-09-04 09:27:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,6 @@ class Config:
     API_ID = int(os.getenv("API_ID", "23889992"))         # your API ID
     API_HASH = os.getenv("API_HASH", "70bf3c9baebf30afff8c32649bf23c3d")  # your API Hash
     BOT_TOKEN = os.getenv("BOT_TOKEN", "")  # your Bot Token
-    DB_URI = os.environ.get("DB_URI", "")         
-    DB_NAME = os.environ.get("DB_NAME", "")
 
     # Other Configs
     PORT = int(os.getenv("PORT", "8080"))
